5a14ac1ae31258f74f5e9650f9cffff0021bf875
Niaina42 <niainachristopher4@gmail.com>
2024-05-04 17:03:49 +0300

    Fix backend

--- backend/src/models/partenaires.ts
+++ backend/src/models/partenaires.ts
@@ -1,94 +1,94 @@
-import { Prisma, PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import { Prisma, PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 const modelPartenaire = {
-    getAll: () => {
-        return prisma.partenaires.findMany({
-            include:{
-                command: {
-                    include: {
-                        user: true
-                    }
-                }
-            }
-        })
-    },
-    getOne: (id: number) =>{
-        return prisma.partenaires.findUnique({
-            where:{id: id},
-            include:{
-                command: {
-                    include: {
-                        user: true
-                    }
-                }
-            }
-        })
-    },
-    create: async(
-        number: number,
-        name: string,
-        image: string,
-        price: number,
-        location: string,
-        description: string,
-        password: string,
-        type: string,
-        email: string
-        ) =>{
-            const res = await prisma.partenaires.create({
-                data:{
-                    number,
-                    name,
-                    email,
-                    image,
-                    price,
-                    location,
-                    description,
-                    password,
-                    type,
-                }
-            })
-            return res
+  getAll: async () => {
+    return prisma.partenaires.findMany({
+      include: {
+        command: {
+          include: {
+            user: true,
+          },
         },
-    update: async(
-        id: number,
-        number: number,
-        name: string,
-        image: string,
-        price: number,
-        location: string,
-        description: string,
-        password: string,
-        type: string,
-        email: string
-        ) =>{
-            const res = await prisma.partenaires.update({
-                where:{id: id},
-                data:{
-                    number,
-                    name,
-                    email,
-                    image,
-                    price,
-                    location,
-                    description,
-                    password,
-                    type,
-                }
-            })
-            return res
+      },
+    });
+  },
+  getOne: async (id: number) => {
+    return prisma.partenaires.findUnique({
+      where: { id: id },
+      include: {
+        command: {
+          include: {
+            user: true,
+          },
         },
-        delete: (id: number)=>{
-            return prisma.partenaires.delete({
-                where:{id: id}
-            })
-        },
-        getByEmail: (email: string)=>{
-            return prisma.partenaires.findUnique({
-                where: {email: email}
-            })
-        }
-}
+      },
+    });
+  },
+  create: async (
+    number: number,
+    name: string,
+    image: string,
+    price: number,
+    location: string,
+    description: string,
+    password: string,
+    type: string,
+    email: string
+  ) => {
+    const res = await prisma.partenaires.create({
+      data: {
+        number,
+        name,
+        email,
+        image,
+        price,
+        location,
+        description,
+        password,
+        type,
+      },
+    });
+    return res;
+  },
+  update: async (
+    id: number,
+    number: number,
+    name: string,
+    image: string,
+    price: number,
+    location: string,
+    description: string,
+    password: string,
+    type: string,
+    email: string
+  ) => {
+    const res = await prisma.partenaires.update({
+      where: { id: id },
+      data: {
+        number,
+        name,
+        email,
+        image,
+        price,
+        location,
+        description,
+        password,
+        type,
+      },
+    });
+    return res;
+  },
+  delete: async (id: number) => {
+    return prisma.partenaires.delete({
+      where: { id: id },
+    });
+  },
+  getByEmail: async (email: string) => {
+    return prisma.partenaires.findUnique({
+      where: { email: email },
+    });
+  },
+};
 
 export default modelPartenaire;

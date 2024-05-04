6c8c4890c315b1b38f95eaf4d28d0798fe401e03
RamamonjizafyAndo <ramamonjizafymanitra06@gmail.com>
2024-05-04 09:01:38 +0300

    add configuration ssl

--- backend/main.ts
+++ backend/main.ts
@@ -2,7 +2,8 @@ import { Response, Request } from "express"
 import express from "express"
 import cors from "cors"
 import userRoute from './src/routes/users'
-
+import https from 'https'
+import fs from 'fs'
 const app = express()
 
 app.use(cors())
@@ -19,6 +20,13 @@ app.get('/api/test',(req:Request, res:Response) => {
   })
 })
 
+const options = {
+  key: fs.readFileSync(__dirname + '/private.key', 'utf8'),
+ cert: fs.readFileSync(__dirname + '/public.cert', 'utf8')
+};
+
 app.use('/api/users', userRoute)
 
-app.listen(9001, () => console.log("Api listen on port 9001"))
+https.createServer(app).listen(9001, function(){
+  console.log("Express server listening on port " + 9001);
+});

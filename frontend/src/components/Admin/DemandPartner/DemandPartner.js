7a241e357e4155f1766abb9a858bb72f2ec38b61
Niaina42 <niainachristopher4@gmail.com>
2024-05-04 15:51:38 +0300

    Fix admin

--- frontend/src/components/Admin/DemandPartner/DemandPartner.js
+++ frontend/src/components/Admin/DemandPartner/DemandPartner.js
@@ -22,53 +22,95 @@ const DemandPartner = () => {
           <div class="row">
             <div class="col-lg-12">
               <div class="card">
-                <div class="card-body">
-                  <h5 class="card-title"></h5>
-                  <table class="table">
+                <div className="card-body">
+                  <h5 className="card-title">
+                    Recent Sales <span>| Today</span>
+                  </h5>
+                  <table className="table table-borderless datatable">
                     <thead>
                       <tr>
                         <th scope="col">#</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Position</th>
-                        <th scope="col">Age</th>
-                        <th scope="col">Start Date</th>
+                        <th scope="col">Customer</th>
+                        <th scope="col">Product</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Status</th>
                       </tr>
                     </thead>
                     <tbody>
                       <tr>
-                        <th scope="row">1</th>
+                        <th scope="row">
+                          <a href="/">#2457</a>
+                        </th>
                         <td>Brandon Jacob</td>
-                        <td>Designer</td>
-                        <td>28</td>
-                        <td>2016-05-25</td>
+                        <td>
+                          <a href="/" className="text-primary">
+                            At praesentium minu
+                          </a>
+                        </td>
+                        <td>$64</td>
+                        <td>
+                          <span className="badge bg-success">Approved</span>
+                        </td>
                       </tr>
                       <tr>
-                        <th scope="row">2</th>
+                        <th scope="row">
+                          <a href="/">#2147</a>
+                        </th>
                         <td>Bridie Kessler</td>
-                        <td>Developer</td>
-                        <td>35</td>
-                        <td>2014-12-05</td>
+                        <td>
+                          <a href="/" className="text-primary">
+                            Blanditiis dolor omnis similique
+                          </a>
+                        </td>
+                        <td>$47</td>
+                        <td>
+                          <span className="badge bg-warning">Pending</span>
+                        </td>
                       </tr>
                       <tr>
-                        <th scope="row">3</th>
+                        <th scope="row">
+                          <a href="/">#2049</a>
+                        </th>
                         <td>Ashleigh Langosh</td>
-                        <td>Finance</td>
-                        <td>45</td>
-                        <td>2011-08-12</td>
+                        <td>
+                          <a href="/" className="text-primary">
+                            At recusandae consectetur
+                          </a>
+                        </td>
+                        <td>$147</td>
+                        <td>
+                          <span className="badge bg-success">Approved</span>
+                        </td>
                       </tr>
                       <tr>
-                        <th scope="row">4</th>
+                        <th scope="row">
+                          <a href="/">#2644</a>
+                        </th>
                         <td>Angus Grady</td>
-                        <td>HR</td>
-                        <td>34</td>
-                        <td>2012-06-11</td>
+                        <td>
+                          <a href="/" className="text-primar">
+                            Ut voluptatem id earum et
+                          </a>
+                        </td>
+                        <td>$67</td>
+                        <td>
+                          <span className="badge bg-danger">Rejected</span>
+                        </td>
                       </tr>
                       <tr>
-                        <th scope="row">5</th>
+                        <th scope="row">
+                          <a href="/">#2644</a>
+                        </th>
                         <td>Raheem Lehner</td>
-                        <td>Dynamic Division Officer</td>
-                        <td>47</td>
-                        <td>2011-04-19</td>
+                        <td>
+                          <a href="/" className="text-primary">
+                            Sunt similique distinctio
+                          </a>
+                        </td>
+                        <td>$165</td>
+                        <td>
+                          <span className="badge bg-success">Approved</span>
+                        </td>
                       </tr>
                     </tbody>
                   </table>
